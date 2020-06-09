9b5bb19f6ea2d04318783e47c73e441d4988831a
Benjamin Swenson <swen0754@umn.edu>
2020-06-09 18:41:59 -0500

    Added HTTP connection and 'Page not Found' error message

--- backend.py
+++ backend.py
@@ -2,8 +2,17 @@ import requests
 
 
 def request_html_https(url):
-    if url[:7] == 'https://':
+    if url[:3] == 'http':
         return requests.get(url).text
     else:
-        url = 'https://' + url
-        return requests.get(url).text
+        url_http = 'http://' + url
+        url_https = 'https://' + url
+
+        try:
+            r = requests.get(url_https).text
+        except:
+            try:
+                r = requests.get(url_http).text
+            except:
+                r = "Error: Page not Found"
+    return r
